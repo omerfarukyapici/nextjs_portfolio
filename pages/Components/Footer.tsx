a6f51bf246092178eed6a040328d6e79ee573a95
omerfarukyapici <omerfarukyapici20@gmail.com>
2022-06-16 21:54:06 +0300

    Changed some files

--- pages/Components/Footer.tsx
+++ pages/Components/Footer.tsx
@@ -1,42 +1,26 @@
-import styled from "styled-components";
-
-const MainDiv = styled.div({
-    backgroundColor: "grey",
-    display: "flex",
-    justifyContent: "center"
-});
-
-const MainUl = styled.ul({
-    display: "flex",
-})
-
-const MainLi = styled.li({
-    listStyle: "none",
-    padding: "1rem",
-
-})
+import Link from "next/link";
 
 const Footer = () => {
     return (
-        <MainDiv>
-            <MainUl>
-                <MainLi>
-                    <a href="">
-                        Linkedin
-                    </a>
-                </MainLi>
-                <MainLi>
-                    <a href="">
-                        Github
-                    </a>
-                </MainLi>
-                <MainLi>
-                    <a href="">
-                        Twitter
-                    </a>
-                </MainLi>
-            </MainUl>
-        </MainDiv>
+        <footer>
+            <div>
+                <div>
+                    <Link href="">
+                        <a>Linkedin</a>
+                    </Link>
+                </div>
+                <div>
+                    <Link href="">
+                        <a>Github</a>
+                    </Link>
+                </div>
+                <div>
+                    <Link href="">
+                        <a>Twitter</a>
+                    </Link>
+                </div>
+            </div>
+        </footer>
     )
 }
 export default Footer;
--- pages/Components/Header.tsx
+++ pages/Components/Header.tsx
@@ -1,66 +1,38 @@
-import styled from "styled-components";
 import Link from "next/link";
-
-/* Styled Components w/elements */
-const H_Section = styled.section`
-    display: flex;
-    justify-content: center;
-    background-color: red;
-`
-const H_Div = styled.div`
-    background-color: yellow;
-    width:  1200px;
-`
-const H_Parent = styled.div`
-    display: flex;
-    justify-content: space-between;
-`
-const H_Link = styled.div`
-    margin: 1.5rem;
-`
-const H_Logo = styled.div`
-    background-color: blue;
-`
-const H_Pages = styled.div`
-    background-color: purple;
-    display: flex;
-`
 
 const Header = () => {
     return (
-        <H_Section>
-            <H_Div>
-                <H_Parent>
-
-                    <H_Logo>
-                        <H_Link>
+        <header>        
+                <div>
+                    
+                    <div>
+                        <div>
                             <Link href="../Home">
                                 <a>Home</a>
                             </Link>
-                        </H_Link>
-                    </H_Logo>
+                        </div>
+                    </div>
 
-                    <H_Pages>
-                        <H_Link>
+                    <div>
+                        <div>
                             <Link href="../Projects">
                                 <a>Projects</a>
                             </Link>
-                        </H_Link>
-                        <H_Link>
+                        </div>
+                        <div>
                             <Link href="../Productivity">
                                 <a>Productivity</a>
                             </Link>
-                        </H_Link>
-                        <H_Link>
+                        </div>
+                        <div>
                             <Link href="../Contact">
                                 <a>Contact</a>
                             </Link>
-                        </H_Link>
-                    </H_Pages>
+                        </div>
+                    </div>
 
-                </H_Parent>
-            </H_Div>
-        </H_Section>
+                </div>
+        </header>
     )
 }
 export default Header;
